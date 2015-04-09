adb10381dd96b683fa9ee5dafc46fd144363c004
Florian Koch <floriankochde@arcor.de>
2015-04-09 11:08:41 +0200

    Added task1 files

---
+++ Task1/adventure.cpp
@@ -0,0 +1,5 @@
+#include "adventure.h"
+
+Adventure::Adventure()
+{
+}
---
+++ Task1/adventure.h
@@ -0,0 +1,10 @@
+#ifndef ADVENTURE_H
+#define ADVENTURE_H
+
+class Adventure
+{
+public:
+    Adventure();
+};
+
+#endif // ADVENTURE_H
